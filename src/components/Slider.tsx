dcabd437d782a436035ca143d45ebb140ab011f9
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-05-21 09:12:22 -0500

    forehand3

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Pagination } from "swiper/modules";
@@ -8,6 +9,8 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import styles from "../styles/index.module.scss";
+import Modal from "react-modal";
+import { motion } from "framer-motion";
 
 interface SliderData {
   image: string;
@@ -16,11 +19,12 @@ interface SliderData {
 
 interface SlideItemProps {
   data: SliderData;
+  onClick: () => void;
 }
 
-const SlideItem: React.FC<SlideItemProps> = ({ data }) => {
+const SlideItem: React.FC<SlideItemProps> = ({ data, onClick }) => {
   return (
-    <main className={styles.sliderContainer}>
+    <main className={styles.sliderContainer} onClick={onClick}>
       <Image src={data.image} alt="slide" width={350} height={400} />
       <h2>{data.h2}</h2>
     </main>
@@ -30,6 +34,29 @@ const SlideItem: React.FC<SlideItemProps> = ({ data }) => {
 interface SliderProps {}
 
 const Slider: React.FC<SliderProps> = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedSlide, setSelectedSlide] = useState<SliderData | null>(null);
+
+  const openModal = (slide: SliderData) => {
+    setSelectedSlide(slide);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedSlide(null);
+  };
+
+
+  const variantsConatc = {
+    hidden: { opacity: 0, x: -150 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.5, ease: "easeInOut" },
+    },
+  };
+
   return (
     <section className={styles.container}>
       <section>
@@ -37,7 +64,7 @@ const Slider: React.FC<SliderProps> = () => {
           effect={"coverflow"}
           grabCursor={true}
           modules={[EffectCoverflow, Pagination]}
-          className="mySwiper"
+          className={`${modalIsOpen ? styles.disableSwiper : ""} mySwiper`}
           spaceBetween={30}
           slidesPerView={1}
           coverflowEffect={{
@@ -51,13 +78,30 @@ const Slider: React.FC<SliderProps> = () => {
         >
           {data.map((item, index) => (
             <SwiperSlide key={index}>
-              <SlideItem data={item} />
+              <SlideItem data={item} onClick={() => openModal(item)} />
             </SwiperSlide>
           ))}
         </Swiper>
       </section>
+
+      {selectedSlide && (
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Slide Modal"
+          className={styles.modal}
+          overlayClassName={styles.overlay}
+        >
+          <div className={styles.modalContent}>
+            <Image src={selectedSlide.image} alt="slide" width={350} height={400} />
+            <h2>{selectedSlide.h2}</h2>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </Modal>
+      )}
     </section>
   );
 };
 
 export default Slider;
+
